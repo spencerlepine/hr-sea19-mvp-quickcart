cbda3b4b1d8357c0c97b66a4c52552b3bc70e069
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-12 10:16:21 -0700

    add frontend boilerplate files (#3)

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="App-footer">
+    <a href="https://spencerlepine.com/">
+      Spencer Lepine
+    </a>
+    <a href="https://github.com/spencerlepine/hr-sea19-mvp">
+      Source Code
+    </a>
+  </footer>
+);
+
+export default Footer;
