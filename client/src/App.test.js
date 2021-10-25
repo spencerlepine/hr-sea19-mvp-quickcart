0684b3b475a09e03eb7c17cae93bbd59c4e74786
Spencer Lepine <spencerlepine26@gmail.com>
2021-10-24 20:23:04 -0700

    Polish the README and add screenshots (#11)
    * fix CI build failure
    
    * add screenshots to README
    
    * add mongo to Action
    
    * add missing file updates
    
    * remove missing server tests
    
    * remove missing server tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Component from './App';
+import { AuthProvider } from './context/AuthContext';
+import {
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 const projectURL = 'https://github.com/spencerlepine/hr-sea19-mvp';
 const projectAuthor = 'Spencer Lepine';
+
+const App = () => (
+  <Router>
+    <AuthProvider>
+      <Component />
+    </AuthProvider>
+  </Router>
+)
 
 test('renders project name', () => {
   render(<App />);
